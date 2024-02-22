472f2877033f9b4b8e197ebb4905951f2337ebd0
Jocelyn Zhao <zhaojocelyn300@gmail.com>
2024-02-21 21:02:55 -0500

    cake

--- interpolate.py
+++ interpolate.py
@@ -4,6 +4,9 @@ from scipy.interpolate import LinearNDInterpolator
 import numpy as np
 import pandas as pd
 import time
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 
 # Next steps:
 # do interpolation (try linear interpolation and grid data) - will take a long time
@@ -16,30 +19,44 @@ csv_file_path = 'final_radiance.csv'
 data = pd.read_csv(csv_file_path)
 
 a = time.time()
-points = data.iloc[:, :4].values
-values = data.iloc[:, 4].values
+# Extract input points and output values
+
+# Extract every 10th row of data
+data_subset = data.iloc[::3, :]
+points = data_subset.iloc[:, :3].values
+output_values = data_subset.iloc[:, 3].values
 b = time.time()
 print(b-a)
 # Create the LinearNDInterpolator
-interp = LinearNDInterpolator(points, values)  #likely have to cut values to make code run faster
+interp = LinearNDInterpolator(points, output_values)
 c = time.time()
-print(c-b)
+print("interp time: " + str(c-b))
+# Define the point to interpolate
 lat = 40.88278
 lon = -98.37418
-hour = 8
-day = 19
+hours_since_start = 20
+point_to_interpolate = [lat, lon, hours_since_start]
 
-# Example of linear interpolation
-example_point = np.array([lat, lon, hour, day])  # Replace with your desired coordinates
-interpolated_value = interp(*example_point)
+# Perform linear interpolation
+interpolated_value = interp(point_to_interpolate)
 d = time.time()
-print(d-c)
+print(f"Interpolated value at {point_to_interpolate}: {interpolated_value}")
+print("time to interpolate one point: " + str(d-c))
 
-print(f"Interpolated value at {example_point}: {interpolated_value}")
+# Create a 3D scatter plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
+# Scatter plot of the original data points
+scatter = ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=output_values, cmap='viridis', marker='o')
 
+# Set labels and title
+ax.set_xlabel('Lat')
+ax.set_ylabel('Lon')
+ax.set_zlabel('Hours from start')
+ax.set_title('Solar slice of cake')
 
+# Add a colorbar
+cbar = fig.colorbar(scatter, ax=ax, orientation='vertical', label='Output Values')
 
-'''
-In my case, points = [lat, lon, hour, day (of the month)]   (for pratical purposes, there should be no need to consider month and year)
-'''
+plt.show()
