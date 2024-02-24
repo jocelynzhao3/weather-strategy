bb707fdb807cfe5cca581bde87f537bd2ea76f26
Jocelyn Zhao <zhaojocelyn300@gmail.com>
2024-02-24 15:37:46 -0500

    interp function

--- weather_interpolate.py
+++ weather_interpolate.py
@@ -1,61 +1,18 @@
-print("Welcome to interpolation!")
+from weather_test import SolarInterpolator
 
-from scipy.interpolate import LinearNDInterpolator
-import numpy as np
-import pandas as pd
-import time
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+#User POV:
+instance = SolarInterpolator("2022_C_tester.csv", "final_radiance_C.csv", 15, 24, 2, 2024)   #hour, day, month, year
+lat_list = [40.8, 40.7, 41.0, 41.8]
+lon_list = [-98.3, -99.1,-100.4, -103.7]
+hours_passed_list = [2, 20, 12, 24]
+radiance_list = instance.interpolate_radiance(lat_list, lon_list, hours_passed_list, write_csv=True)
+print(radiance_list)
 
+'''
+Notes:
+1. interp list of points seems pretty fast (first query ~ 3 sec, after that <<0.01 sec)
+2. creating interpolator takes ~10 sec (done once)
+3. writing csv takes multiple minutes (1 segment ~ 8 minutes)
+4. points outside of interpolation WILL RETURN NAN - user needs to expect that or I take care of it
 
-# Next steps:
-# make interpolation function
-# how to get stuff into MIT SEVT github?
-# try a server-client with another machine
-
-# Read CSV file
-csv_file_path = 'final_radiance.csv'
-data = pd.read_csv(csv_file_path)
-
-a = time.time()
-# Extract input points and output values
-
-# Extract every 10th row of data
-data_subset = data.iloc[::3, :]
-points = data_subset.iloc[:, :3].values
-output_values = data_subset.iloc[:, 3].values
-b = time.time()
-print(b-a)
-# Create the LinearNDInterpolator
-interp = LinearNDInterpolator(points, output_values)
-c = time.time()
-print("interp time: " + str(c-b))
-# Define the point to interpolate
-lat = 40.88278
-lon = -98.37418
-hours_since_start = 20
-point_to_interpolate = [lat, lon, hours_since_start]
-
-# Perform linear interpolation
-interpolated_value = interp(point_to_interpolate)
-d = time.time()
-print(f"Interpolated value at {point_to_interpolate}: {interpolated_value}")
-print("time to interpolate one point: " + str(d-c))
-
-# Create a 3D scatter plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-# Scatter plot of the original data points
-scatter = ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=output_values, cmap='viridis', marker='o')
-
-# Set labels and title
-ax.set_xlabel('Lat')
-ax.set_ylabel('Lon')
-ax.set_zlabel('Hours from start')
-ax.set_title('Solar slice of cake')
-
-# Add a colorbar
-cbar = fig.colorbar(scatter, ax=ax, orientation='vertical', label='Output Values')
-
-plt.show()
+'''
